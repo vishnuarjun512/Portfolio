3b4c545e761d27884e85b4b303525f0d40d886c3
vishnuarjun512 <vishnuarjun512@gmail.com>
2023-10-24 14:03:42 +0530

    Base Project, about to add Darkmode

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import Contact from "./components/Contact";
-import DarkMode from "./components/DarkMode";
 import Main from "./components/Main";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Resume from "./components/Resume";
-
 import SideNav from "./components/SideNav";
 import React from "react";
 
